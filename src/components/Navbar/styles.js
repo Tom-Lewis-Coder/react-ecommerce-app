fc4f077dfddc76368fa113284b9e5deb3885a1d1
Tom-Lewis-Coder <tomlewis139@gmail.com>
2022-12-30 16:44:55 +0000

    removed unused css

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,4 +1,4 @@
-import { makeStyles, alpha } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 
 const drawerWidth = 0
 
@@ -26,49 +26,7 @@ export default makeStyles((theme) => ({
         textDecoration: 'none',
         color: 'black',
     },
-    menuButton: {
-        marginRight: theme.spacing(2),
-        [theme.breakpoints.up('sm')]: {
-            display: 'none',
-        },
-    },
     grow: {
         flexGrow: 1,
-    },
-    search: {
-        position: 'relative',
-        borderRdius: theme.shape.borderRadius,
-        backgroundColor: 'black',
-        '&:hover': {
-            backgroundColor: alpha(theme.palette.common.white, 0.25),
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: 'auto',
-        }, 
-    },
-    searchIcon: {
-        padding: theme.spacing(0,2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1,1,1,0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(40)}px)`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
-        },
     },
 }))
