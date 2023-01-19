9bd3d37a26aef9dcaea120c382c6312943763328
Tom-Lewis-Coder <tomlewis139@gmail.com>
2023-01-19 15:24:00 +0000

    removed stlyes.css

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Typography, Button } from '@material-ui/core'
 import { Link } from 'react-router-dom'
-// import './styles.css'
 
 import useStyles from './styles'
 
